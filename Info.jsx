348c22e4a5e1b8a73013140b9f55c3fa2e640f2f
umamanaaz554 <84959117+umamanaaz554@users.noreply.github.com>
2021-06-28 21:59:53 +0530

    Create Info.jsx

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+ return(
+   <div className="note">
+  <h1>Javascript and react.js</h1>
+  <p>Basic web dev reactjs Bootcamp</p>
+  </div>
+ );
+}
+
+export default Info;
